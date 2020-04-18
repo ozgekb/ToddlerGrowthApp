4c9f97a648065ae5e6835d5fd75d8431cb853490
ozgekb <ozge.kantar@gmail.com>
2020-04-17 20:07:36 -0700

    trying to leanr git

---
+++ ToddlerGrowthApp/ToddlerGrowthApp/Baby.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ToddlerGrowthApp
+{
+    public class Baby
+    {
+        public Baby()
+        {
+        }
+    }
+}
---
+++ ToddlerGrowthApp/ToddlerGrowthApp/EmptyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ToddlerGrowthApp
+{
+    public class EmptyClass
+    {
+        public EmptyClass()
+        {
+        }
+    }
+}
