9d8ed9a30e63f3e51d8c2d7efe335a525c0ed2a9
ozgekb <ozge.kantar@gmail.com>
2020-04-18 00:01:52 -0700

    added more properties and classes

--- ToddlerGrowthApp/ToddlerGrowthApp/Baby.cs
+++ ToddlerGrowthApp/ToddlerGrowthApp/Baby.cs
@@ -1,10 +1,32 @@
 ﻿using System;
 namespace ToddlerGrowthApp
 {
-    public class Baby
+    /// <summary>
+    /// this class represent the baby who is growt rate will be trak
+    /// </summary>
+    class Baby
     {
-        public Baby()
-        {
-        }
+        #region Properties
+        public string BabyName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string BirthPlace { get; set; }
+        public DateTime BirthTime { get; set; }
+        public decimal BirthWeight { get; set; }
+        public WeightRecord[] WeightRecords;
+        public decimal BirthHeight { get; set; }
+        public decimal CurrentHeight { get; set; }
+        public decimal BirthHeadCircumference { get; set; }
+        public decimal CurrentHeadCircumference { get; set; }
+        public string BirthHospital { get; set; }
+        public string DoctorName { get; set; }
+        public string[] BabyPictures { get; set; }
+        public string[] MedicalRecords { get; set; }
+        public SleepingTrack SleepingTrack;
+        public FeedingTrack FeedingTrack;
+        #endregion
+        #region Methods
+
+
+        #endregion
     }
 }
---
+++ ToddlerGrowthApp/ToddlerGrowthApp/SleepingTrack.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ToddlerGrowthApp
+{
+    public class SleepingTrack
+    {
+
+
+        Methods
+        public bool EnterSleepingTrack(DateTime StartTime, DateTime EndTime);// there is a red line and I didnt understand why?
+        {
+            
+        }
+}
+}
